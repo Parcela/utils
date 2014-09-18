4106c60f6f0627352baf9f92bc4838b59c3b8e30
Marco Asbreuk <marco@itsasbreuk.nl>
2014-09-18 14:12:19 +0200

    making test pass IE9
    Also added tests for typeOf

--- tests/idGenerator.js
+++ tests/idGenerator.js
@@ -1,14 +1,16 @@
 /*global describe, it */
 "use strict";
-var should = require('chai').should();
+var expect = require('chai').expect,
+    should = require('chai').should();
+
 var generator = require('../index.js').idGenerator;
 
 describe('Testing unique generated Id\'s', function () {
     it('first item without namespace --> 1', function () {
-        generator().should.be.eql(1);
+        expect(generator()).to.eql(1);
     });
     it('second item without namespace --> 2', function () {
-        generator().should.be.eql(2);
+        expect(generator()).to.eql(2);
     });
     it('first item with namespace --> dom-1', function () {
         generator('dom').should.be.eql('dom-1');
@@ -17,25 +19,25 @@ describe('Testing unique generated Id\'s', function () {
         generator('dom').should.be.eql('dom-2');
     });
     it('third item without namespace and after another namespace --> 3', function () {
-        generator().should.be.eql(3);
+        expect(generator()).to.eql(3);
     });
     it('third item with namespace and after another namespace --> dom-3', function () {
         generator('dom').should.be.eql('dom-3');
     });
     it('item without namespace with start specified --> 1000', function () {
-        generator(1000).should.be.eql(1000);
+        expect(generator(1000)).to.eql(1000);
     });
     it('item with namespace with start specified --> dom-500', function () {
         generator('dom', 500).should.be.eql('dom-500');
     });
     it('item without namespace after start specified --> 1001', function () {
-        generator().should.be.eql(1001);
+        expect(generator()).to.eql(1001);
     });
     it('item with namespace after start specified --> dom-501', function () {
         generator('dom').should.be.eql('dom-501');
     });
     it('item without namespace with invalid start specified --> 1002', function () {
-        generator(700).should.be.eql(1002);
+        expect(generator(700)).to.eql(1002);
     });
     it('item with namespace with invalid start specified --> dom-502', function () {
         generator('dom', 300).should.be.eql('dom-502');
